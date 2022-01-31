9ae8788153c62a0710af1b4316694805b9ace48a
Thomas Kavoo <kingkavoh@gmail.com>
2022-01-30 20:02:34 -0600

    Parsing file

--- mysci.py
+++ mysci.py
@@ -1,6 +1,28 @@
+#Initialize my data variable
+data = []
+
+
 # Read the data file
 filename = "data/wxobs20170821.txt"
 
 with open(filename, 'r') as datafile:
-    data = datafile.read() #with the datafile open, I would want to print the data
+    
+    #read the first three lines (header)
+    for _ in range(3):
+        datafile.readline()
 
+
+    #Read nand parse the rest of the file
+    for line in datafile:
+        datum = line.split()
+        data.append(datum)
+
+
+# DEBUG
+#print(data[8][4])
+#print(data[8][:5])
+#print(data[8][::2])
+
+
+#print(data[5:8][0])
+#print(data[5])
