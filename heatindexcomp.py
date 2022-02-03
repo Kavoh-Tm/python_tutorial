4c16ad28acf1838697f95a38656e42e74801827f
Thomas Kavoo <kingkavoh@gmail.com>
2022-02-03 15:20:35 -0600

    Refactoring scripts to using the model

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,3 +1,5 @@
+from readdata import read_data
+
 # Column names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'heatindex' : 13}
 
@@ -14,24 +16,7 @@ for column in columns:
 
 
 # Read the data file
-filename = "data/wxobs20170821.txt"
-
-with open(filename, 'r') as datafile:
-
-    #read the first three lines (header)
-    for _ in range(3):
-        datafile.readline()
-        
-
-
-    #Read and parse the rest of the file
-    for line in datafile:
-        split_line = line.split()
-        for column in columns:
-            i = columns[column]
-            t = types.get(column, str)
-            value = t(split_line[i])
-            data[column].append(value)
+data = read_data(columns, types=types)
 
 # compute the wind chill temperature
 def compute_heatindex(t, rh_pct):
