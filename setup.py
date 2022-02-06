0b1cd084ffcccf2672d762a79c4f09dd722ac92f
Thomas Kavoo <kingkavoh@gmail.com>
2022-02-05 22:18:45 -0600

    Adding setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+	name="mysci",
+	version="1.0.0",
+	description="A sample package",
+	author="Kavoh",
+	author_email="kavoh@edu.com",
+	packages=["mysci"],
+	Install_requires=[])
